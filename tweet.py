efee13cc63c9754c6febc7e8aa416267490cc0f5
Alex Zavalny <57161600+thelazyaz@users.noreply.github.com>
2022-04-05 00:17:44 -0400

    Add files via upload

---
+++ tweet.py
@@ -0,0 +1,25 @@
+import tweepy
+import json
+
+key = open('api_key.txt','r')
+
+keys = key.readlines()
+
+API_KEY = keys[0].strip()
+API_SECRET = keys[1].strip()
+ACCESS_TOKEN = keys[2].strip()
+ACCESS_SECRET = keys[3].strip()
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+def get_tweets(user, howmany):
+    tweets = api.user_timeline(screen_name=user, count=howmany, include_rts = True)#max tweets is 200
+    return tweets
+
+
+def tweet_response_to_jsonfile(tweets):
+    with open('tweet.json', 'w', encoding='utf-8') as f:
+        json.dump(tweets[0]._json, f, ensure_ascii=False, indent=4)
