e6b3285275b3be51b89fa398f5e6262bce1a0488
John Khachian <jaksawesome10@gmail.com>
2022-04-08 18:05:21 -0400

    see if this works

--- app.py
+++ app.py
@@ -41,6 +41,5 @@ def results():
 
 if __name__ == '__main__':
   
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
